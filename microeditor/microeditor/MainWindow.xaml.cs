2df5743039b3445ed891aa1363d8669a4b327396
Alumno <Alumno@PC16>
2019-10-09 12:39:12 -0500

    nah

---
+++ microeditor/microeditor/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace microeditor
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtEditor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int row = txtEditor.GetLineIndexFromCharacterIndex(txtEditor.CaretIndex);
+            int col = txtEditor.CaretIndex - txtEditor.GetCharacterIndexFromLineIndex(row);
+            lbCursorPosition.Text = "Linea " + (row + 1) + ", Letra " + (col + 1);
+            PbLleno.Value = col + 10;
+        }
+    }
+}
